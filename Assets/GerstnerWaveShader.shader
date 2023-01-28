9039fe33526d29eb03c67808fff8f1660a18ab42
j22-robertson <55785515+j22-robertson@users.noreply.github.com>
2023-01-28 22:56:19 +0000

    added shader parameters to C# script for designers

--- Assets/GerstnerWaveShader.shader
+++ Assets/GerstnerWaveShader.shader
@@ -25,12 +25,12 @@ Shader "Custom/GerstnerWaveShader"
     }
     SubShader
     {
-        Tags { "RenderType"="Transparent" "Queue" = "Transparent" }
+        Tags { "RenderType"="Opaque"}
         LOD 200
 
         CGPROGRAM
         // Physically based Standard lighting model, and enable shadows on all light types
-        #pragma surface surf Standard alpha vertex:vert addshadow
+        #pragma surface surf Standard fullforwardshadows vertex:vert addshadow
         #include "GerstnerWave.cginc"
         #include "Flow.cginc"
         // Use shader model 3.0 target, to get nicer looking lighting
--- Assets/water_manager.cs
+++ Assets/water_manager.cs
@@ -4,13 +4,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class watersplash : MonoBehaviour
+[RequireComponent(typeof(MeshRenderer))]
+public class water_manager : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private ParticleSystem SplashParticles;
     [SerializeField] private BoxCollider _collider;
+
+    [SerializeField] private Vector4 WaveA;
+    [SerializeField] private Vector4 WaveB;
+    [SerializeField] private Vector4 WaveC;
+    private MeshRenderer _meshRenderer;
+
+    [SerializeField] private float _Tiling = 1;
+    //[SerializeField] private float Speed = 1;
     void Start()
     {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        
         
     }
 
@@ -33,6 +44,9 @@ public class watersplash : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        _meshRenderer.material.SetFloat("_Tiling", _Tiling);
+        _meshRenderer.material.SetVector("_WaveA", WaveA);
+        _meshRenderer.material.SetVector("_WaveB", WaveB);
+        _meshRenderer.material.SetVector("_WaveC", WaveC);
     }
 }
